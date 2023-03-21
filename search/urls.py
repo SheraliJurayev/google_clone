cc1a2d495dfef5eae7ca5029cdaacc1167cbd25d
SheraliJurayev <sheralijurayev0412@gmail.com>
2023-03-21 08:50:51 +0500

    install requests , bs4 and 1xml

--- search/urls.py
+++ search/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path('' ,  views.index , name="index") , 
+    path('' ,  views.search , name="search") , 
+    
 
 ]
--- search/views.py
+++ search/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+import requests
+from bs4 import BeautifulSoup as bs
 
 # Create your views here.
 def index(request):
     return render(request , "index.html")
+
+def search(request):
+    if request.method == 'POST':  
+        search = request.POST['search']
+        url = "https://www.ask.com/web?q=freecodecamp"
+    return render(request , "search.html")
